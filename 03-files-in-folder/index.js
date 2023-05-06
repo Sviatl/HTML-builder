f75b5024727112b2f3c063a81efc07e19e1e24d2
Sviatlana Yurusava <sv.yurusova@gmail.com>
2023-05-06 11:54:55 +0300

    feat: ready rask 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+let directoryPath = 'secret-folder';
+directoryPath = path.join(__dirname, directoryPath);
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.log(`Ошибка при чтении директории: ${err}`);
+  } else {
+    console.log(`Файлы в директории ${directoryPath}:`);
+    files.forEach((file) => {
+      const filePath = `${directoryPath}/${file}`;
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.log(`Ошибка при чтении файла ${filePath}: ${err}`);
+        } else {
+          if (stats.isFile()) {
+            const fileName = path.parse(filePath).name;
+            const fileExt = path.parse(filePath).ext;
+            const fileSize = stats.size;
+            console.log(`${fileName} - ${fileExt.replace(/^\./, '')} - ${fileSize/1000}kb`);
+          }
+        }
+      });
+    });
+  }
+});
